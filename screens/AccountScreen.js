b227e8c9b76d9fb342d3b0c68d8735887ac977e6
Roshni Soni <roshni.soni44@gmail.com>
2023-05-16 11:49:30 -0700

    Account Option List

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { AuthContext } from '../store/auth-context';
 import { useContext } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const accountOptions = [
+  {
+    key: "1",
+    text: "Profile",
+    icon: "account"
+  },
+  {
+    key: "2",
+	  text: "Manage Address",
+    icon: "map-marker"
+  },
+  {
+	  key: "3",
+	  text: "My Subscriptions",
+    icon: "calendar-month"
+  },
+  {
+	  key: "4",
+	  text: "Orders",
+    icon: "cart"
+  },
+  {
+	  key: "5",
+	  text: "Manage Notifications",
+    icon: "bell"
+  },
+  {
+	  key: "6",
+	  text: "Payments",
+    icon: "currency-usd"
+  },
+  {
+	  key: "7",
+	  text: "Help / FAQ",
+    icon: "help-circle-outline"
+  }
+];
+
+const myItemSeparator = () => {
+  return <View style={{ height: 2, backgroundColor: "grey", marginHorizontal:0}} />;
+  };
 
 function AccountScreen() {
-  const authCtx = useContext(AuthContext);
-  const name = authCtx.name;
+   const authCtx = useContext(AuthContext);
+   const name = authCtx.name;
   return (
-    <View style={styles.rootContainer}>
-      <Text style={styles.title}>Account</Text>
-      
+    <View>
+      <FlatList 
+        data = {accountOptions}
+        renderItem = {(itemData) =>{
+          return (
+            <View>
+              <Text style={styles.listOptions}> 
+              <MaterialCommunityIcons name={itemData.item.icon} size={32} color="black"/>
+              {"   "}
+              {itemData.item.text} </Text>
+            </View>
+          );
+        }}
+        ItemSeparatorComponent={myItemSeparator}
+        alwaysBounceVertical = {false}
+      />
     </View>
   
   );
@@ -17,15 +73,11 @@ function AccountScreen() {
 export default AccountScreen;
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 32,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
+  
+  listOptions: {
+    fontSize: 18,
+    marginTop: 6,
+    fontWeight: "bold",
+    padding: 20
+  }
 });
