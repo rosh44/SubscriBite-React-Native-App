f0a171cd99715210ac1f14ff124e92cf623153a6
Aman Thakkar <aman.thakkar@somaiya.edu>
2023-06-14 15:34:38 -0700

    Past subscriptions made dynamic

--- screens/PastOrdersScreen.js
+++ screens/PastOrdersScreen.js
@@ -1,86 +1,82 @@
-import { useState, useEffect} from 'react';
-import {
-  Text,
-  View,
-  FlatList,
-  StyleSheet,
-} from 'react-native';
+import { useState, useEffect, useContext } from 'react';
+import { Text, View, FlatList, StyleSheet } from 'react-native';
 import axios from 'axios';
+import { AuthContext } from '../store/auth-context';
 
 export default function PastordersScreen() {
+  const authCtx = useContext(AuthContext);
+  const [userId, setUserId] = useState(authCtx.localId);
 
-  const [userId, setUserId] = useState('6011');
-  
   const [data, setData] = useState([]);
   useEffect(() => {
-  const fetchData = async () => {
-    console.log('Fetching data...');
-    const url =
-      'http://dev-lb-subscribite-234585004.us-west-2.elb.amazonaws.com/subscriptions/past_orders'; // Replace with your API endpoint URL
-    const finalData = [];
+    const fetchData = async () => {
+      console.log('Fetching data...');
+      const url =
+        'http://dev-lb-subscribite-234585004.us-west-2.elb.amazonaws.com/subscriptions/past_orders'; // Replace with your API endpoint URL
+      const finalData = [];
 
-    try {
-      const response = await axios.post(url, { user_id: userId });
-      // setData(response.data); // Assuming the API response is an array of items
-      const res = response.data;
-      // console.log(res);
-      // console.log(JSON.stringify(response.data));
-      for (const date in res) {
-        // for every date, get the associated items
-        const items = res[date];
-        // console.log(items);
-        // for every item in the array, add a newItem object to finalData
-        items.forEach((item) => {
-          const newItem = {
-            id: item.id,
-            date: item.delivery_date,
-            timeslot: item.time_slot_id,
-            timeslot_description: item.time_slot_description,
-            name: item.name,
-            price: item.price,
-            quantity: item.quantity,
-            price: item.price,
+      try {
+        const response = await axios.post(url, { user_id: userId });
+        // setData(response.data); // Assuming the API response is an array of items
+        const res = response.data;
+        // console.log(res);
+        // console.log(JSON.stringify(response.data));
+        for (const date in res) {
+          // for every date, get the associated items
+          const items = res[date];
+          // console.log(items);
+          // for every item in the array, add a newItem object to finalData
+          items.forEach((item) => {
+            const newItem = {
+              id: item.id,
+              date: item.delivery_date,
+              timeslot: item.time_slot_id,
+              timeslot_description: item.time_slot_description,
+              name: item.name,
+              price: item.price,
+              quantity: item.quantity,
+              price: item.price,
+            };
+            finalData.push(newItem);
+          });
+        }
+        setData([...finalData]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      // console.log(finalData[0]);
+
+      // finalData is now an array of indidivudal delivery items, transform it to the desired map
+      const result = [];
+
+      finalData.forEach((item) => {
+        const { date, timeslot, timeslot_description, price } = item;
+        const dateOnly = new Date(date).toISOString().split('T')[0]; // Extracting only the date part
+        const key = `${dateOnly}__${timeslot}`;
+        const existingEntry = result.find((entry) => entry.key === key);
+
+        if (existingEntry) {
+          existingEntry.items.push(item);
+          existingEntry.totalCost += price;
+        } else {
+          let newEntry = {
+            key: key,
+            dateOnly: dateOnly,
+            timeslot: timeslot_description,
+            items: [item],
+            totalCost: price,
           };
-          finalData.push(newItem);
-        });
-      }
-      setData([...finalData]);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-    // console.log(finalData[0]);
+          result.push(newEntry);
+        }
+      });
 
-    // finalData is now an array of indidivudal delivery items, transform it to the desired map
-    const result = [];
+      // console.log(result);
 
-    finalData.forEach((item) => {
-      const { date, timeslot, timeslot_description, price } = item;
-      const dateOnly = new Date(date).toISOString().split('T')[0]; // Extracting only the date part
-      const key = `${dateOnly}__${timeslot}`;
-      const existingEntry = result.find((entry) => entry.key === key);
-
-      if (existingEntry) {
-        existingEntry.items.push(item);
-        existingEntry.totalCost += price;
-      } else {
-        let newEntry = {
-          key: key,
-          dateOnly: dateOnly,
-          timeslot: timeslot_description,
-          items: [item],
-          totalCost: price,
-        };
-        result.push(newEntry);
-      }
-    });
-
-    // console.log(result);
-
-    setData([...result]);
-  };
+      setData([...result]);
+    };
     fetchData();
-   }, []);
- 
+  }, []);
+
   const renderItem = ({ item }) => {
     return (
       <View style={styles.itemContainer}>
@@ -148,7 +144,7 @@ const styles = StyleSheet.create({
   keyText: {
     fontSize: 16,
     fontWeight: 'bold',
-    marginBottom: 5
+    marginBottom: 5,
   },
   keyText1: {
     fontSize: 13.5,
